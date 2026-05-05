a8dbd940a29725914e75a9f02b03fb30884376ce
muafalah <muh.ifa.amrillah@gmail.com>
2026-05-05 20:08:59 +0700

    fix: ensure workflow execution cleanup in e2e tests and integrate Elasticsearch into the workflow execution module

--- src/modules/workflow-execution/workflow-execution.module.ts
+++ src/modules/workflow-execution/workflow-execution.module.ts
@@ -3,12 +3,14 @@ import { BullModule } from '@nestjs/bullmq';
 import { WorkflowExecutionService } from './workflow-execution.service';
 import { WorkflowExecutionProcessor } from './workflow-execution.processor';
 import { WorkflowRunGateway } from './workflow-run.gateway';
+import { ElasticsearchModule } from '../elasticsearch/elasticsearch.module';
 
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'workflow-execution',
     }),
+    ElasticsearchModule,
   ],
   providers: [
     WorkflowExecutionService,
